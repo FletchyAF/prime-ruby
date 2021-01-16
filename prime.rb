420636adc688dd20a3a27b311b59adb624658b4b
FletchyAF <joshua.f1998@gmail.com>
2021-01-16 17:43:58 -0500

    commit tree 0e97120d425a987c0722a930466c8b96948073af

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+    divisor = 2
+     if number == 2
+        return true
+    end
+
+    if number <= 1
+        return false
+    end
+   
+    for divisor in 2..(number - 1)
+        if number % divisor == 0
+            return false        
+        end   
+    end
+    return true
+end
